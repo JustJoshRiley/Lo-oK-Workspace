6fe4bb42efb3d1718ece596b1e5a61f62679c4b3
justjoshriley <joshriley316@gmail.com>
2022-01-18 16:14:23 -0800

    removing firefly-react debug

--- src/Components/Hero/hero.js
+++ src/Components/Hero/hero.js
@@ -3,12 +3,10 @@ import './hero.css';
 
 function Hero() {
     return(
-        <>
         <div className="HeroContainer">
             <h1 className="Header">( L<a className="header-o">o</a>-<a className="header-o">o</a>K )</h1>
             <h2 className="Subtext">A New Way To Search</h2>
         </div>
-        </>
     )
 }
 
